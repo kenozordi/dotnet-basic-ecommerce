28add10d743ccb99fa6a732ef3246b5ce92fd675
kennyozordi <kennyozordi@gmail.com>
2023-02-10 09:48:57 +0100

    Let there be light

---
+++ dotnet-basic-ecommerce/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace dotnet_basic_ecommerce.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
